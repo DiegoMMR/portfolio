77f94017261d4d402219b51ca9902a373f29e2df
Diego M. Rivera <mrdiego.dev@gmail.com>
2025-06-17 17:45:55 -0600

    Updates meta description to reflect title
    Updates the meta description to be more concise and reflect the title of the page.
    Removes redundant word in the description to keep it accurate.

--- app.vue
+++ app.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import LanguageSelector from "@/components/LanguageSelector.vue";
-import Header from "@/components/Header/index.vue";
-import About from "@/components/Content/About/index.vue";
-import Experience from "@/components/Content/Experience/index.vue";
-import Projects from "@/components/Content/Projects/index.vue";
-import { options } from "@/constants/particles";
+  import LanguageSelector from '@/components/LanguageSelector.vue';
+  import Header from '@/components/Header/index.vue';
+  import About from '@/components/Content/About/index.vue';
+  import Experience from '@/components/Content/Experience/index.vue';
+  import Projects from '@/components/Content/Projects/index.vue';
+  import { options } from '@/constants/particles';
 
-useHead({
-  htmlAttrs: {
-    class: "scroll-smooth focus:scroll-auto",
-  },
-  bodyAttrs: {
-    class: "leading-relaxed text-white bg-soft-black",
-  },
-});
+  useHead({
+    htmlAttrs: {
+      class: 'scroll-smooth focus:scroll-auto',
+    },
+    bodyAttrs: {
+      class: 'leading-relaxed text-white bg-soft-black',
+    },
+  });
 
-useSeoMeta({
-  title: "Diego M. Rivera",
-  description: "Diego M. Rivera's personal Portfolio. A Full Stack Developer.",
-  ogDescription: "Diego M. Rivera's personal Portfolio. A Full Stack Developer.",
-  keywords: "Diego M. Rivera, Full Stack Developer, Software Engineer, Frontend Developer",
-});
+  useSeoMeta({
+    title: 'Diego M. Rivera',
+    description: "Diego M. Rivera's personal Portfolio. A Software Engineer.",
+    ogDescription: "Diego M. Rivera's personal Portfolio. A Software Engineer.",
+    keywords: 'Diego M. Rivera, Full Stack Developer, Software Engineer, Frontend Developer',
+  });
 </script>
 
 <template>
